f6762f593087bb965087b5512fc2acbb2a29d5b8
DamirF <d.fatkhullin@mail.ru>
2022-03-20 15:55:39 +0300

    Frequency Analysis for words(symbols)

---
+++ MainForm.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace FrequencyAnalysis
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.CloseAppBut = new System.Windows.Forms.Button();
+            this.PlainText = new System.Windows.Forms.RichTextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.AnalyzeSentencesType = new System.Windows.Forms.Button();
+            this.AnalyzeWordsType = new System.Windows.Forms.Button();
+            this.AnalyzeSymbolsType = new System.Windows.Forms.Button();
+            this.AnalyzeGistogrammEn = new System.Windows.Forms.PictureBox();
+            this.AnalyzeGistogrammRus = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.AnalyzeGistogrammEn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AnalyzeGistogrammRus)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.CloseAppBut);
+            this.panel1.Location = new System.Drawing.Point(0, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1920, 38);
+            this.panel1.TabIndex = 0;
+            // 
+            // CloseAppBut
+            // 
+            this.CloseAppBut.BackColor = System.Drawing.Color.Red;
+            this.CloseAppBut.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.CloseAppBut.Location = new System.Drawing.Point(1882, 0);
+            this.CloseAppBut.Name = "CloseAppBut";
+            this.CloseAppBut.Size = new System.Drawing.Size(38, 38);
+            this.CloseAppBut.TabIndex = 0;
+            this.CloseAppBut.Text = "x";
+            this.CloseAppBut.UseVisualStyleBackColor = false;
+            this.CloseAppBut.Click += new System.EventHandler(this.CloseAppBut_Click);
+            // 
+            // PlainText
+            // 
+            this.PlainText.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.PlainText.Location = new System.Drawing.Point(12, 42);
+            this.PlainText.Name = "PlainText";
+            this.PlainText.Size = new System.Drawing.Size(848, 955);
+            this.PlainText.TabIndex = 1;
+            this.PlainText.Text = "";
+            this.PlainText.TextChanged += new System.EventHandler(this.PlainText_TextChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.AnalyzeSentencesType);
+            this.panel2.Controls.Add(this.AnalyzeWordsType);
+            this.panel2.Controls.Add(this.AnalyzeSymbolsType);
+            this.panel2.Location = new System.Drawing.Point(12, 1003);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(848, 65);
+            this.panel2.TabIndex = 2;
+            // 
+            // AnalyzeSentencesType
+            // 
+            this.AnalyzeSentencesType.Location = new System.Drawing.Point(610, 3);
+            this.AnalyzeSentencesType.Name = "AnalyzeSentencesType";
+            this.AnalyzeSentencesType.Size = new System.Drawing.Size(237, 57);
+            this.AnalyzeSentencesType.TabIndex = 2;
+            this.AnalyzeSentencesType.Text = "По Предложениям";
+            this.AnalyzeSentencesType.UseVisualStyleBackColor = true;
+            this.AnalyzeSentencesType.Click += new System.EventHandler(this.AnalyzeSentencesType_Click);
+            // 
+            // AnalyzeWordsType
+            // 
+            this.AnalyzeWordsType.Location = new System.Drawing.Point(307, 3);
+            this.AnalyzeWordsType.Name = "AnalyzeWordsType";
+            this.AnalyzeWordsType.Size = new System.Drawing.Size(237, 57);
+            this.AnalyzeWordsType.TabIndex = 1;
+            this.AnalyzeWordsType.Text = "По Словам";
+            this.AnalyzeWordsType.UseVisualStyleBackColor = true;
+            this.AnalyzeWordsType.Click += new System.EventHandler(this.AnalyzeWordsType_Click);
+            // 
+            // AnalyzeSymbolsType
+            // 
+            this.AnalyzeSymbolsType.Location = new System.Drawing.Point(3, 3);
+            this.AnalyzeSymbolsType.Name = "AnalyzeSymbolsType";
+            this.AnalyzeSymbolsType.Size = new System.Drawing.Size(237, 57);
+            this.AnalyzeSymbolsType.TabIndex = 0;
+            this.AnalyzeSymbolsType.Text = "По Буквам";
+            this.AnalyzeSymbolsType.UseVisualStyleBackColor = true;
+            this.AnalyzeSymbolsType.Click += new System.EventHandler(this.AnalyzeSymbolsType_Click);
+            // 
+            // AnalyzeGistogrammEn
+            // 
+            this.AnalyzeGistogrammEn.Location = new System.Drawing.Point(930, 42);
+            this.AnalyzeGistogrammEn.Name = "AnalyzeGistogrammEn";
+            this.AnalyzeGistogrammEn.Size = new System.Drawing.Size(960, 490);
+            this.AnalyzeGistogrammEn.TabIndex = 3;
+            this.AnalyzeGistogrammEn.TabStop = false;
+            // 
+            // AnalyzeGistogrammRus
+            // 
+            this.AnalyzeGistogrammRus.Location = new System.Drawing.Point(930, 574);
+            this.AnalyzeGistogrammRus.Name = "AnalyzeGistogrammRus";
+            this.AnalyzeGistogrammRus.Size = new System.Drawing.Size(960, 490);
+            this.AnalyzeGistogrammRus.TabIndex = 4;
+            this.AnalyzeGistogrammRus.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1920, 1080);
+            this.Controls.Add(this.AnalyzeGistogrammRus);
+            this.Controls.Add(this.AnalyzeGistogrammEn);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.PlainText);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.AnalyzeGistogrammEn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AnalyzeGistogrammRus)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button CloseAppBut;
+        private System.Windows.Forms.RichTextBox PlainText;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button AnalyzeSentencesType;
+        private System.Windows.Forms.Button AnalyzeWordsType;
+        private System.Windows.Forms.Button AnalyzeSymbolsType;
+        private System.Windows.Forms.PictureBox AnalyzeGistogrammEn;
+        private System.Windows.Forms.PictureBox AnalyzeGistogrammRus;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrequencyAnalysis
+{
+    public partial class MainForm : Form
+    {
+        private const double MARGIN = 0.05;
+        private const int EN_ALPH_FIRST_WORD = 65;
+        private const int EN_ALPH_LAST_WORD = 90;
+        private const int RUS_ALPH_FIRST_WORD = 1040;
+        private const int RUS_ALPH_LAST_WORD = 1071;
+
+        private readonly Point START_POINT;
+
+        private Graphics g;
+        private Pen pen;
+        private Dictionary<char, int> EnSymbolsCount = new Dictionary<char, int>();
+        private Dictionary<char, int> RusSymbolsCount = new Dictionary<char, int>();
+        private List<string> TextWords = new List<string>();
+        private Bitmap gistogrammEn, gistogrammRus;
+        private int EnMaxValue, RusMaxValue;
+        private int scaleXEn, scaleYEn, scaleXRus, scaleYRus;
+        private string plainText;
+        
+
+        public MainForm()
+        {
+            InitializeComponent();
+            pen = new Pen(Color.Black, 2f);
+            START_POINT = new Point(
+                Convert.ToInt32(AnalyzeGistogrammEn.Width * MARGIN),
+                Convert.ToInt32(AnalyzeGistogrammEn.Height * MARGIN));
+        }
+
+        private void CloseAppBut_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            gistogrammEn = new Bitmap(AnalyzeGistogrammEn.Width, AnalyzeGistogrammEn.Height);
+            gistogrammRus = new Bitmap(AnalyzeGistogrammRus.Width, AnalyzeGistogrammRus.Height);
+            ResetGistogramm(gistogrammEn, AnalyzeGistogrammEn);
+            ResetGistogramm(gistogrammRus, AnalyzeGistogrammRus);
+            
+        }
+
+        private void PlainText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AnalyzeSymbolsType_Click(object sender, EventArgs e)
+        {
+            ResetGistogramm(gistogrammEn, AnalyzeGistogrammEn);
+            ResetGistogramm(gistogrammRus, AnalyzeGistogrammRus);
+            SymbolsCountCalculate();
+            BuildGistogrammForSymbols(EnSymbolsCount, EnMaxValue, gistogrammEn, AnalyzeGistogrammEn);
+            BuildGistogrammForSymbols(RusSymbolsCount, RusMaxValue, gistogrammRus, AnalyzeGistogrammRus);
+        }
+
+        private void AnalyzeWordsType_Click(object sender, EventArgs e)
+        {
+            WordsCountCalculate();
+        }
+
+        private void AnalyzeSentencesType_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Обновление поля для гистограммы
+        /// </summary>
+        /// <param name="map"></param>
+        /// <param name="PB"></param>
+        private void ResetGistogramm(Bitmap map, PictureBox PB)
+        {
+            for(int i = 0; i < map.Width; i++)
+            {
+                for (int j = 0; j < map.Height; j++)
+                {
+                    map.SetPixel(i, j, Color.White);
+                }
+            }
+            DrawAxis(map);
+            PB.Image = map;
+        }
+
+        /// <summary>
+        /// Рисование осей гистограммы
+        /// </summary>
+        /// <param name="map"></param>
+        private void DrawAxis(Bitmap map)
+        {
+            g = Graphics.FromImage(map);
+            g.DrawLine(
+                pen,
+                new Point(Convert.ToInt32(map.Width * MARGIN), Convert.ToInt32(map.Height * (1 - MARGIN))),
+                new Point(Convert.ToInt32(map.Width * (1 - MARGIN)), Convert.ToInt32(map.Height * (1 - MARGIN)))
+                );
+        }
+
+        /// <summary>
+        /// Инициализация и заполнение словарей для частотного анализа по буквам
+        /// </summary>
+        private void InitDictionaryForSymbols()
+        {
+            EnSymbolsCount.Clear();
+            RusSymbolsCount.Clear();
+            for(int i = EN_ALPH_FIRST_WORD; i <= EN_ALPH_LAST_WORD; i++)
+            {
+                EnSymbolsCount.Add((char)i, 0);
+            }
+            for(int i = RUS_ALPH_FIRST_WORD; i <= RUS_ALPH_LAST_WORD; i++)
+            {
+                RusSymbolsCount.Add(Convert.ToChar(i), 0);
+            }
+        }
+
+        /// <summary>
+        /// Подсчет букв в тексте и количества наиболее встречаемой буквы
+        /// </summary>
+        private void SymbolsCountCalculate()
+        {
+            EnMaxValue = 0;
+            RusMaxValue = 0;
+            InitDictionaryForSymbols();
+            plainText = PlainText.Text.ToUpper();
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                if (plainText[i] >= EN_ALPH_FIRST_WORD && plainText[i] <= EN_ALPH_LAST_WORD)
+                    EnSymbolsCount[plainText[i]]++;
+                else if (plainText[i] >= RUS_ALPH_FIRST_WORD && plainText[i] <= RUS_ALPH_LAST_WORD)
+                    RusSymbolsCount[plainText[i]]++;
+            }
+
+            for (int i = EN_ALPH_FIRST_WORD; i <= EN_ALPH_LAST_WORD; i++)
+            {
+                if (EnSymbolsCount[(char)i] > EnMaxValue) EnMaxValue = EnSymbolsCount[(char)i];
+            }
+
+            for (int i = RUS_ALPH_FIRST_WORD; i <= RUS_ALPH_LAST_WORD; i++)
+            {
+                if (RusSymbolsCount[(char)i] > RusMaxValue) RusMaxValue = RusSymbolsCount[(char)i];
+            }
+        }
+
+        /// <summary>
+        /// Построение гистограммы частотного анализа по буквам
+        /// </summary>
+        /// <param name="alphSymbolsCount"></param>
+        /// <param name="MaxCount"></param>
+        /// <param name="alphMap"></param>
+        /// <param name="PB"></param>
+
+        private void BuildGistogrammForSymbols(Dictionary<char, int> alphSymbolsCount, int MaxCount, Bitmap alphMap, PictureBox PB)
+        {
+            if(MaxCount == 0) return;
+            int scaleX = Convert.ToInt32(alphMap.Width * 0.9 / alphSymbolsCount.Count);
+            int scaleY = Convert.ToInt32(alphMap.Height * 0.9 / MaxCount);
+            g = Graphics.FromImage(alphMap);
+            List<int> values = alphSymbolsCount.Values.ToList();
+            List<char> keys = alphSymbolsCount.Keys.ToList();
+            for (int i = 0; i < values.Count; i++)
+            {
+                int x = START_POINT.X + scaleX * i;
+                int y = ((MaxCount - values[i]) * scaleY + START_POINT.Y) ;
+                int height = Convert.ToInt32(alphMap.Height * (1-  2 * MARGIN)) - y + START_POINT.Y;
+                if (values[i] != 0) g.DrawRectangle(pen, new Rectangle(x, y, 10, height));
+                else y = Convert.ToInt32(alphMap.Height * (1 - MARGIN));
+                g.DrawString(Convert.ToString(keys[i]), new Font("Arial", 9), Brushes.Black, new Point(START_POINT.X + scaleX * i, alphMap.Height - 20));
+                g.DrawString(Convert.ToString(values[i].ToString()), new Font("Arial", 9), Brushes.Black, new Point(START_POINT.X + scaleX * i, y - 20));
+
+            }
+            g.Dispose();
+            PB.Image = alphMap;
+        }
+
+        private void WordsCountCalculate()
+        {
+            plainText = PlainText.Text;
+            char[] separators = new char[] { ' ', '.', ',', '<', '>', '"', ':', ';', '-', '+', '=', '\'', '/', '?', '!', '@', '*' };
+
+            TextWords = plainText.Split(separators, StringSplitOptions.RemoveEmptyEntries).ToList();
+        }
+    }
+}
